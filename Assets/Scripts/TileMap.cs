ad335ad1c35824b10d3fe8c5bc1725e1eb9f9c1a
Tsinoyboi <ajsnoface@gmail.com>
2015-04-08 23:00:01 -0700

    a
    a

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class TileMap : MonoBehaviour {
 
@@ -18,8 +19,9 @@ public class TileMap : MonoBehaviour {
     void Start()
     {
         GenerateMapData();
-        GeneratePathfindingGraph();
         GenerateMapVisuals();
+        //GeneratePathfindingGraph();
+        
     }
 
     void GenerateMapData()
@@ -62,9 +64,21 @@ public class TileMap : MonoBehaviour {
     class Node
     {
         public List<Node> neighbors;
+        public int x;
+        public int y;
+
         public Node()
         {
             neighbors = new List<Node>();
+        }
+
+        public float DistanceTo(Node n)
+        {
+            return Vector2.Distance
+                (
+                new Vector2(x, y),
+                new Vector2(n.x, n.y)
+                );
         }
 
     }
@@ -77,23 +91,18 @@ public class TileMap : MonoBehaviour {
         {
             for (int y = 0; y < mapSizeX; y++)
             {
-                // 4 way connected
+                graph[x, y].x = x;
+                graph[x, y].y = y;
+
+                //// 4 way connected
                 if (0 < x)
-                {
                     graph[x, y].neighbors.Add(graph[x - 1, y]);
-                }
                 if (mapSizeX - 1 > x)
-                {
                     graph[x, y].neighbors.Add(graph[x + 1, y]);
-                }
                 if (0 < y)
-                {
                     graph[x, y].neighbors.Add(graph[x, y - 1]);
-                }
                 if (mapSizeY - 1 > y)
-                {
                     graph[x, y].neighbors.Add(graph[x, y + 1]);
-                }
             }
         }
     }
@@ -116,15 +125,60 @@ public class TileMap : MonoBehaviour {
 
     public Vector3 TileToWorldCoord(int x, int y)
     {
-        return new Vector3(x, 1, y);
+        return new Vector3(x, 2, y);
     }
 
     public void MoveSelectedUnitTo(int x, int y)
     {
+        
         selectedUnit.GetComponent<Unit>().tileX = x;
         selectedUnit.GetComponent<Unit>().tileY = y;
         selectedUnit.transform.position = TileToWorldCoord(x, y);
         selectedUnit.transform.rotation = Quaternion.identity;
+        
+        /*
+        Dictionary<Node, float> dist = new Dictionary<Node, float>();
+        Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
+
+        // setup the queue - unchecked nodes
+        List<Node> unvisited = new List<Node>();
+
+        Node source = graph[
+            selectedUnit.GetComponent<Unit>().tileX,
+            selectedUnit.GetComponent<Unit>().tileY
+            ];
+        dist[source] = 0;
+        prev[source] = null;
+
+        foreach (Node v in graph)
+        {
+            if (v != source)
+            {
+                dist[v] = Mathf.Infinity;
+                prev[v] = null;
+            }
+            unvisited.Add(v);
+        }
+
+        while(unvisited.Count > 0)
+        {
+            // not fast but short
+            // consitter priority of other self sorting optimized
+            Node u = unvisited.OrderBy(n => dist[n]).First();
+            unvisited.Remove(u);
+            foreach (Node v in u.neighbors)
+            {
+                float alt = dist[u] + u.DistanceTo(v);
+                if (alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = u;
+                }
+            }
+
+        }
+
+        */
     }
 
 }
